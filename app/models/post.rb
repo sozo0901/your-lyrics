0bb08992c1cb3880ba4de72f9beef68f596206e8
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-04 22:58:41 +0000

    [Add] association and posts_controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # Postモデルに対して、Userモデルとの関係性を追加
+  # belongs_toは、Postモデルからuser_idに関連付けられていて、Userモデルを参照することができる。
+  # Postモデルに関連付けられるのは、1つのUserモデル。このため、単数形の「user」になっている。
+  belongs_to :user
 end
