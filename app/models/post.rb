401e17cbea6bab78506bfb2e4f021c1da05392a0
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-05 02:13:22 +0000

    [Add] Likes table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ApplicationRecord
-  # Postモデルに対して、Userモデルとの関係性を追加。
   # belongs_toは、Postモデルからuser_idに関連付けられていて、Userモデルを参照することができる。
   # Postモデルに関連付けられるのは、1つのUserモデル。このため、単数形の「user」になっている。
   belongs_to :user
+  # # Postモデルに対して、Likeモデルが1:Nになるように関連付け
+  has_many :likes, dependent: :destroy
+  # liked_by?メソッドで、引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる。
+  # 存在していればtrue、存在していなければfalseを返す。
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
   # モデルに、画像アップ用のメソッド（attachment）を追加してフィールド名に（image）を指定。
   # refileを使用するうえでのルール。(idは含めない)
   attachment :image
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # Userモデルに対して、Postモデルが1:Nになるよう関連付け
+  # Userモデルに対して、Postモデルが1:Nになるように関連付け
   has_many :posts, dependent: :destroy
+  # Userモデルに対して、Likeモデルが1:Nになるように関連付け
+  has_many :likes, dependent: :destroy
+  
 end
