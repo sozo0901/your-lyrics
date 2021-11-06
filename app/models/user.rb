46728ec4b6e5639782fa44d93fe7172f8ac18a2d
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-06 13:55:45 +0000

    [Fix] layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  validates :name, presence: true
+  validates :email, presence: true
   # Userモデルに対して、Postモデルが1:Nになるように関連付け
   has_many :posts, dependent: :destroy
   # Userモデルに対して、Likeモデルが1:Nになるように関連付け
