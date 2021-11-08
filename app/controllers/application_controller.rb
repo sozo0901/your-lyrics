09d98c803dd9e7932034f01884f77655e98f89ec
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-08 09:07:44 +0000

    [Add] redirect_posts action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     posts_path
   end
-
+  
   # protectedは呼び出された他のコントローラからも参照できる
   protected
 
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
+  before_action :redirect_posts
+
+  # ユーザーがログインしている場合、トップページへは遷移できず投稿一覧画面を表示させる
+  def redirect_posts
+    if user_signed_in?
+      redirect_to posts_path
+    end
+  end
+
   def top
   end
 end
