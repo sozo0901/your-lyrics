c2740d9b4feba00e9cea114ecbdde96350aa2986
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-08 12:30:17 +0000

    [Update] like ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.new(post_id: @post.id)
     like.save
     # app/views/likes/create.js.erbを参照する
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    like = current_user.likes.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.find_by(post_id: @post.id)
     like.destroy
     # app/views/likes/destroy.js.erbを参照する
   end
