43ce67b52db3301142c0089eb2bbb264ff7a1da0
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-08 12:51:38 +0000

    [Add] bookmark ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,14 +1,14 @@
 class StocksController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    stock = current_user.stocks.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    stock = current_user.stocks.new(post_id: @post.id)
     stock.save
     # app/views/stocks/create.js.erbを参照する
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    stock = current_user.stocks.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    stock = current_user.stocks.find_by(post_id: @post.id)
     stock.destroy
     # app/views/stocks/destroy.js.erbを参照する
   end
