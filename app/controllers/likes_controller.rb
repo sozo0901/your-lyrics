687e247b44df1c699f4736b75e7f057b46da325a
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-08 10:23:48 +0000

    [Update] likes and stocks controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,14 @@ class LikesController < ApplicationController
     post = Post.find(params[:post_id])
     like = current_user.likes.new(post_id: post.id)
     like.save
-    redirect_to request.referer
+    # app/views/likes/create.js.erbを参照する
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: post.id)
     like.destroy
-    redirect_to request.referer
+    # app/views/likes/destroy.js.erbを参照する
   end
 
 end
