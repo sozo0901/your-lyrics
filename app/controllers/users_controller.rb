7962e667b93e861dfa1990f6cf0f63c4a366c616
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-10 14:47:09 +0000

    [Add] user-destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
   private
 
   def user_params
