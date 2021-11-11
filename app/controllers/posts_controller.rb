9cda664461b5d2a681596f9259772ebe6533c93e
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-11 07:52:19 +0000

    [Update]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    # データを新規登録するためのインスタンス作成
     @post = Post.new(post_params)
-    # current_userはdeviseのヘルパーメソッド。deviseを導入しないと使用できない。
     @post.user_id = current_user.id
-   
+
     if @post.save
       redirect_to posts_path
     else
@@ -17,12 +15,24 @@ class PostsController < ApplicationController
   end
 
   def index
-    # タイムライン上に表示する投稿データを取得
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -10,12 +10,12 @@ class RelationsController < ApplicationController
   end
 
   def following
-     user = User.find(params[:user_id])
-     @users = user.following
+     @user = User.find(params[:user_id])
+     @users = @user.following
   end
 
   def followers
-     user = User.find(params[:user_id])
-     @users = user.followers
+     @user = User.find(params[:user_id])
+     @users = @user.followers
   end
 end
