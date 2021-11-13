a645d1590fda0223564e5e1f910b9e069410d5fd
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-13 07:02:13 +0000

    [Delete] チャット機能と不要ファイル

--- app/models/room.rb
+++
@@ -1,4 +0,0 @@
-class Room < ApplicationRecord
-  has_many :chats, dependent: :destroy
-  has_many :user_rooms, dependent: :destroy
-end
--- db/migrate/20211107084406_entries.rb
+++
@@ -1,5 +0,0 @@
-class Entries < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :entries
-  end
-end
