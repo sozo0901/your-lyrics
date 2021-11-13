f2cbd0634ff574b4cde01cabf763117993fc8508
sozo0901 <taiki.ishikawa.97@gmail.com>
2021-11-13 11:52:23 +0000

    [Add] フォロー中ユーザーの投稿一覧

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy]
     resource :stocks, only: [:create, :destroy]
   end
+  get 'following-posts' => 'posts#following_posts', as: 'following_posts'
+
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resource :relations, only: [:create, :destroy]
     get 'following' => 'relations#following', as: 'following'
